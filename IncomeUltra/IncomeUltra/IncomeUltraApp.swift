97ec28fe243088faafea94d23b2601afdcb399ed
Nidhishree <nayaknidhishree06@gmail.com>
2026-02-13 17:56:36 +0530

    Initial Xcode commit.

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Nidhishree Nayak on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
